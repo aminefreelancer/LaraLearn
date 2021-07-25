a104abd333e6adb9bad171054bc93cce691e5b93
Amine Akacha <akachamohamedamine@gmail.com>
2021-07-25 09:10:24 +0100

    Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,14 @@ Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
 
 Route::get('/hello', [TestController::class, 'index']);
 
+Route::get('/user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post: '.$postId.' / Comment: '.$commentId;
+});
+
 Route::get('/greeting', function () {
     return 'Hello World';
 });
