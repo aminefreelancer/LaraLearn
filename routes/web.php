6fa57dd75c145de5d1c72b3e7291fccfeb369c53
Amine Akacha <akachamohamedamine@gmail.com>
2021-07-25 09:04:19 +0100

    Routing to a Single Controller Method

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,8 @@ Route::get('/', function () {
 
 Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
 
+
+Route::get('/hello', [TestController::class, 'index']);
 
 Route::get('/greeting', function () {
     return 'Hello World';
