ec93536dc05a250d8f6fd585d09692a1f6b3f84e
Amine Akacha <akachamohamedamine@gmail.com>
2021-07-25 08:55:59 +0100

    Callback Functions and Route::view()

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
